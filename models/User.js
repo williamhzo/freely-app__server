b32d7e6923014cb4d414361b8d58bd91665a04e1
William Hermozo <william.hermozo@protonmail.com>
2020-05-14 10:18:11 +0200

    add User model

--- models/User.js
+++ models/User.js
@@ -1,9 +1,36 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Skills = require('./Skills')
+const Collabs = require('./Collabs')
 
 const userSchema = new Schema({
+  name: { type: String, required: true }
+  userName: { type: String, required: true }
   email: { type: String, required: true },
   password: { type: String, required: true },
+  phone: Number,
+  profilePicture: {type: String, default: 'https://image.flaticon.com/icons/png/512/1738/1738760.png'},
+  title: String,
+  bio: String,
+  socialLinks: [String],
+  location: String,
+  formattedLocation: String,
+  remote: Boolean,
+  private: Boolean,
+  portfolio:{
+    image:{type: String, default:'https://images.pexels.com/photos/3850200/pexels-photo-3850200.jpeg?cs=srgb&dl=modern-workplace-with-gadgets-and-notebook-on-marble-table-3850200.jpg&fm=jpg'},
+    description: String,
+    title: String,
+    link: String,
+  },
+  id_skills: {
+    type: Schema.Types.ObjectId,
+    ref: 'Skills',
+  },
+  id_collabs: {
+    type: Schema.Types.ObjectId,
+    ref: 'Collabs',
+  },
 });
 
 const User = mongoose.model("User", userSchema);
