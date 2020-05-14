591ff2352ec3dc905f15572682615019afd677df
Samuel Littlefair-Wallace <sam@littlefairwallace.ca>
2020-05-14 12:47:42 +0200

    Create get all categories and get all skills routes

--- routes/categories.js
+++ routes/categories.js
@@ -2,4 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Category = require("./../models/Category");
 
+router.get("/", (req, res, next) => {
+  Category.find({})
+    .then((dbRes) => res.status(200).json(dbRes))
+    .catch((err) => res.status(500).json(err));
+});
+
 module.exports = router;
--- routes/skills.js
+++ routes/skills.js
@@ -2,4 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Skill = require("./../models/Skill");
 
+router.get("/", (req, res, next) => {
+  Skill.find({})
+    .then((dbRes) => res.status(200).json(dbRes))
+    .catch((err) => res.status(500).json(err));
+});
+
 module.exports = router;
