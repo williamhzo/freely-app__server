8c47914671586b4e61f3f572a0f9f7378432cf30
William Hermozo <william.hermozo@protonmail.com>
2020-05-19 20:46:53 +0200

    remove console.log

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,28 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const User = require("../models/User");
-const Category = require("../models/Category");
-const Skill = require("../models/Skill");
-const Collab = require("../models/Collab");
-const multer = require("multer");
+const User = require('../models/User');
+const Category = require('../models/Category');
+const Skill = require('../models/Skill');
+const Collab = require('../models/Collab');
+const multer = require('multer');
 const upload = multer();
-const uploadCloud = require("../config/cloudinaryConfig.js");
-const updateFilters = require("./../bin/updateFilters.js");
+const uploadCloud = require('../config/cloudinaryConfig.js');
+const updateFilters = require('./../bin/updateFilters.js');
 
 // Get all users
 
-router.get("/", (req, res, next) => {
+router.get('/', (req, res, next) => {
   User.find(req.query)
-    .populate({ path: "userCategory", model: Category })
-    .populate({ path: "userSkills", model: Skill })
-    .populate({ path: "userCollab", model: Collab })
+    .populate({ path: 'userCategory', model: Category })
+    .populate({ path: 'userSkills', model: Skill })
+    .populate({ path: 'userCollab', model: Collab })
     .then((dbRes) => res.status(200).json(dbRes))
     .catch((err) => res.status(500).json(err));
 });
 
 // Create one user
 
-router.post("/", uploadCloud.single("image"), (req, res, next) => {
+router.post('/', uploadCloud.single('image'), (req, res, next) => {
   User.create(req.body)
     .then((dbRes) => res.status(200).json(dbRes))
     .catch((err) => res.status(500).json(err));
@@ -30,11 +30,11 @@ router.post("/", uploadCloud.single("image"), (req, res, next) => {
 
 // Get one user
 
-router.get("/:id", (req, res, next) => {
+router.get('/:id', (req, res, next) => {
   User.findById(req.params.id)
-    .populate({ path: "userCategory", model: Category })
-    .populate({ path: "userSkills", model: Skill })
-    .populate({ path: "userCollab", model: Collab })
+    .populate({ path: 'userCategory', model: Category })
+    .populate({ path: 'userSkills', model: Skill })
+    .populate({ path: 'userCollab', model: Collab })
     .then((dbRes) => {
       res.status(200).json(dbRes);
     })
@@ -43,7 +43,7 @@ router.get("/:id", (req, res, next) => {
 
 // Edit one user
 
-router.patch("/:id", uploadCloud.any(), (req, res, next) => {
+router.patch('/:id', uploadCloud.any(), (req, res, next) => {
   if (req.body.portfolio) {
     req.body.portfolio = JSON.parse(req.body.portfolio);
   }
@@ -59,30 +59,30 @@ router.patch("/:id", uploadCloud.any(), (req, res, next) => {
   if (req.files) {
     req.files.forEach((file) => {
       switch (file.fieldname) {
-        case "portfolio0":
+        case 'portfolio0':
           req.body.portfolio[0].image = file.secure_url;
           break;
-        case "portfolio1":
+        case 'portfolio1':
           req.body.portfolio[1].image = file.secure_url;
           break;
-        case "portfolio2":
+        case 'portfolio2':
           req.body.portfolio[2].image = file.secure_url;
           break;
-        case "profilePicture":
+        case 'profilePicture':
           req.body.profilePicture = file.secure_url;
           break;
       }
     });
     // console.log(req.files);
-    console.log(req.body);
+    // console.log(req.body);
     // req.body.profilePicture = req.file.secure_url;
   }
   User.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   })
-    .populate({ path: "userCategory", model: Category })
-    .populate({ path: "userSkills", model: Skill })
-    .populate({ path: "userCollab", model: Collab })
+    .populate({ path: 'userCategory', model: Category })
+    .populate({ path: 'userSkills', model: Skill })
+    .populate({ path: 'userCollab', model: Collab })
     .then((dbRes) => res.status(200).json(dbRes))
     .catch((err) => console.log(err));
   updateFilters.categories();
@@ -96,7 +96,7 @@ router.patch("/:id", uploadCloud.any(), (req, res, next) => {
 
 // Delete one user
 
-router.delete("/:id", (req, res, next) => {
+router.delete('/:id', (req, res, next) => {
   User.findByIdAndDelete(req.params.id)
     .then((dbRes) => res.status(200).json(dbRes))
     .catch((err) => console.log(err));
