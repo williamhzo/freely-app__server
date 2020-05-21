72b0c1c9fe8f2a7572eed314b0eaf6a7f43c04b1
William Hermozo <william.hermozo@protonmail.com>
2020-05-22 00:37:55 +0200

    deploy heroku

---
+++ public/precache-manifest.0f745a46847cf7f8ab29f95357457d48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5701be25bf5ca7a3db7dcd8e9c3bb38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "615eea51cbd1add82fb9",
+    "url": "/static/css/main.10f7940f.chunk.css"
+  },
+  {
+    "revision": "f743e174fecabb13bf2e",
+    "url": "/static/js/2.8eb62d99.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.8eb62d99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "615eea51cbd1add82fb9",
+    "url": "/static/js/main.b06934b9.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
