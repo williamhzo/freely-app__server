f03117a0bfaf7d4d14814b625998bc37f9836e05
Samuel Littlefair-Wallace <sam@littlefairwallace.ca>
2020-05-21 23:23:13 +0200

    Deploy merge

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,18 +11,14 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
 importScripts(
-<<<<<<< HEAD
-  "/precache-manifest.2ef952441cd55139212fa775d01ff854.js"
-=======
-  "/precache-manifest.4bf161e99f54a00cf8be2b5df8f3f1ab.js"
->>>>>>> 7ca3830c1045f9c550e63640ece0c0e07333c191
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
+importScripts("/precache-manifest.2ef952441cd55139212fa775d01ff854.js");
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
@@ -37,7 +33,9 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
-});
+workbox.routing.registerNavigationRoute(
+  workbox.precaching.getCacheKeyForURL("/index.html"),
+  {
+    blacklist: [/^\/_/, /\/[^\/?]+\.[^\/]+$/],
+  }
+);
